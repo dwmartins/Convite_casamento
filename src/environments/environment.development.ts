b70525d1b2313fa31058394ae3a392debde3f974
Douglas Martins <douglas5422@outlook.com>
2023-09-02 22:41:43 -0300

    fix: correções

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,3 @@
 export const environment = {
-    production: false,
     API: `https://cd4f-200-52-29-78.ngrok-free.app`
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-    production: true,
     API: `https://cd4f-200-52-29-78.ngrok-free.app`
 };
