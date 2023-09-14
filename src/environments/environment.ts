581a545c004b3ed7bb17279a5474af229ef42249
Douglas Martins <douglas5422@outlook.com>
2023-09-13 23:42:47 -0300

    fix: Corrigindo o link da api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    API: `https://164a-2804-58-c123-be00-e491-b4e8-4890-b9b9.ngrok-free.app/`
+    API: `https://164a-2804-58-c123-be00-e491-b4e8-4890-b9b9.ngrok-free.app`
 };
